4841ebd16b99bfb0d81b3d386920bc9fec0c0d17
Cole Williams <cowi4030@colorado.edu>
2016-09-16 18:58:38 -0700

    testing functionality updated for components in Data folder

--- test/App.spec.js
+++ test/App.spec.js
@@ -4,8 +4,8 @@ const React = require('react')
 const { mount, shallow } = require('enzyme')
 import  ColNavBar from '../client/components/Dashboard/Data/ColNavBar'
 import ApiSandBox from '../client/components/Dashboard/Data/ApiSandBox'
-
-
+import DataItem from '../client/components/Dashboard/Data/DataItem'
+import DBNavBar from '../client/components/Dashboard/Data/DBNavBar'
 
 describe('test test 12 12 />', () => {
 	it('should pass test', () => {
@@ -14,11 +14,14 @@ describe('test test 12 12 />', () => {
 })
 
 describe('ColNavbar />', function() {
+	const names=[1,2,3,4]
 	it('should render div with className = display', function() { 
-		const names=[1,2,3,4]
-		const wrapper = shallow(<ColNavBar names={names} />)
-		console.log('wrapper', wrapper)
+		const wrapper = shallow(<ColNavBar names={names}/>)
 		expect(wrapper.find('div .display')).to.have.length(1)
+	})
+	it('should have props = names', function() {
+		const wrapper = shallow(<ColNavBar names={names}/>)
+		expect(wrapper.props().names).to.be.defined
 	})
 })
 
@@ -27,4 +30,30 @@ describe('ApiSandBox />', function() {
 		const wrapper = shallow(<ApiSandBox />)
 		expect(wrapper.find('input')).to.have.length(4)
 	})
+	it('should render div with className=display jumbotron', function() {
+		const wrapper = shallow(<ApiSandBox />)
+		expect(wrapper.find('div .display jumbotron'))
+	})
 })
+
+describe('DataItem />', function() {
+	it('should have props = info', function() {
+		const wrapper = shallow(<DataItem />)
+		expect(wrapper.props().info).to.be.defined
+	})
+	it('should render a list item with className=i list-group-item', function() {
+		const wrapper = shallow(<DataItem />)
+		expect(wrapper.find('li .i list-group-item'))
+	})
+})
+
+describe('DBNavBar />', () => {
+	const names = ['h', 'e', 'l', 'l', 'o']
+	it('should have props = names', () => {
+		const wrapper = shallow(<DBNavBar names={names}/>)
+		expect(wrapper.props().names).to.be.defined
+	})
+})
+
+
+
