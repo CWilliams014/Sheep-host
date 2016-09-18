6fea43a434030ed82fbec5f9fdbc38f836fa9e3d
Cole Williams <cowi4030@colorado.edu>
2016-09-18 15:30:49 -0700

    Data component tests refactored

--- test/App.spec.js
+++ test/App.spec.js
@@ -6,6 +6,9 @@ import  ColNavBar from '../client/components/Dashboard/Data/ColNavBar'
 import ApiSandBox from '../client/components/Dashboard/Data/ApiSandBox'
 import DataItem from '../client/components/Dashboard/Data/DataItem'
 import DBNavBar from '../client/components/Dashboard/Data/DBNavBar'
+import Display from '../client/components/Dashboard/Data/Display'
+
+// Tests for all components in Data folder
 
 describe('test test 12 12 />', () => {
 	it('should pass test', () => {
@@ -30,7 +33,7 @@ describe('ApiSandBox />', function() {
 		const wrapper = shallow(<ApiSandBox />)
 		expect(wrapper.find('input')).to.have.length(4)
 	})
-	it('should render div with className=display jumbotron', function() {
+	it('should render div with className = display jumbotron', function() {
 		const wrapper = shallow(<ApiSandBox />)
 		expect(wrapper.find('div .display jumbotron'))
 	})
@@ -41,9 +44,10 @@ describe('DataItem />', function() {
 		const wrapper = shallow(<DataItem />)
 		expect(wrapper.props().info).to.be.defined
 	})
-	it('should render a list item with className=i list-group-item', function() {
-		const wrapper = shallow(<DataItem />)
-		expect(wrapper.find('li .i list-group-item'))
+	it('should render a list item with className = i list-group-item', function() {
+		const info = [3,4,5,6]
+		const wrapper = shallow(<DataItem info={info}/>)
+		expect(wrapper.find('li')).to.have.length(1)
 	})
 })
 
@@ -52,6 +56,22 @@ describe('DBNavBar />', () => {
 	it('should have props = names', () => {
 		const wrapper = shallow(<DBNavBar names={names}/>)
 		expect(wrapper.props().names).to.be.defined
+	})
+	it('should return proper amount of button tabs', () => {
+		const wrapper = shallow(<DBNavBar names={names}/>)
+		expect(wrapper.find('button')).to.have.length(5)
+	})
+})
+
+describe('Display />', () => {
+	const display=[1,2,3,4,5]
+	it('should have props = display', () => {
+		const wrapper = shallow(<Display display={display}/>)
+		expect(wrapper.props().display).to.be.defined
+	})
+	it('should render an unordered list', () => {
+		const wrapper = shallow(<Display display={display}/>)
+		expect(wrapper.find('ul')).to.have.length(1)
 	})
 })
 
